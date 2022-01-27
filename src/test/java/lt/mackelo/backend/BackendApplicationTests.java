ea1251970e2c78312efdeb7b3656d14ee505b500
Haroldas Mackelo <h.mackelo@icloud.com>
2022-01-27 09:35:22 +0200

    Initial commit.

---
+++ src/test/java/lt/mackelo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.mackelo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
