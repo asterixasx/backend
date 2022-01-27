0c0afa8cba5e7008be47158f9fab0a21e2d49e02
Haroldas Mackelo <h.mackelo@icloud.com>
2022-01-27 23:54:22 +0200

    Added HTTPS.

--- src/main/java/lt/mackelo/backend/BackendApplication.java
+++ src/main/java/lt/mackelo/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package lt.mackelo.backend;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		//SpringApplication.run(BackendApplication.class, args);
+		SpringApplication application = new SpringApplication(BackendApplication.class);
+		application.setAdditionalProfiles("ssl");
+		application.run(args);
 	}
 }
